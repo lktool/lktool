cb9bb00dc07d41604fe547be533509741fcfe8cb
Mat-project <mathan21092006@gmail.com>
2025-04-30 20:38:09 +0530

    speed 21

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
-from rest_framework import status, views
+from rest_framework import generics, status, permissions
 from rest_framework.response import Response
-from django.contrib.auth import authenticate
-from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.views import APIView
 from rest_framework_simplejwt.views import TokenObtainPairView
 from django.contrib.auth import get_user_model
 from django.contrib.auth.tokens import default_token_generator
