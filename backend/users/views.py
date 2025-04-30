8feac1a2bb23478e90176e429f9dfe89a6c2fc05
Mat-project <mathan21092006@gmail.com>
2025-04-30 20:14:56 +0530

    speed 18

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
-from rest_framework import generics, status, permissions
-from rest_framework.views import APIView
+from rest_framework import status, permissions, generics
 from rest_framework.response import Response
-from rest_framework.exceptions import AuthenticationFailed
+from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.views import TokenObtainPairView
 from django.contrib.auth import get_user_model
 from django.contrib.auth.tokens import default_token_generator
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
